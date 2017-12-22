d51e1df2009fefe58f442aca483d08856dffb8c3
Dave Banerjee <dvbnrg@gmail.com>
2017-12-22 00:51:13 -0600

    implicit variable declarations

--- src/github.com/justdave/littlegobook.com/main.go
+++ src/github.com/justdave/littlegobook.com/main.go
@@ -5,8 +5,13 @@ import (
 )
 
 func main() {
-	var power int
-	power = 9000
+	power := 1000
 	fmt.Printf("It's over %d\n", power)
 
+	name, power := "Goku", getPower()
+	fmt.Printf("%s's power is over %d\n", name, power)
+}
+
+func getPower() int {
+	return 9001
 }
