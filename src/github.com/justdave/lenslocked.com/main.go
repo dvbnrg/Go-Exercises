365b19f245bf920c5c9c290b2384a756bf2f9587
Dave Banerjee <dvbnrg@gmail.com>
2018-01-02 17:19:57 -0600

    another practice API

--- src/github.com/justdave/lenslocked.com/main.go
+++ src/github.com/justdave/lenslocked.com/main.go
@@ -9,8 +9,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View    //This is a frowned upon global variable, TODO: refactor dis
-var contactView *views.View //This is a frowned upon global variable, TODO: refactor dis
+var homeView *views.View
+var contactView *views.View
 
 func main() {
 
