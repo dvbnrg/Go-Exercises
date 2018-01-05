66b23ccf612739b764ff9da9ff02204b73520384
Dave Banerjee <dvbnrg@gmail.com>
2018-01-04 21:05:34 -0600

    Revert

--- src/github.com/justdave/lenslocked.com/main.go
+++ src/github.com/justdave/lenslocked.com/main.go
@@ -2,61 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
-
-	"github.com/justdave/lenslocked.com/views"
 
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
-var contactView *views.View
-
-func main() {
-
-	homeView = views.NewView("bootstrap",
-		"views/home.gohtml")
-	contactView = views.NewView("bootstrap",
-		"views/contact.gohtml")
-
-	var h http.Handler = http.HandlerFunc(home)
-	r := mux.NewRouter()
-	// This will assign the home page to the
-	// NotFoundHandler
-	r.NotFoundHandler = h
-	r.HandleFunc("/faq", faq)
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/hiross", hiross)
-	http.ListenAndServe(":3000", r)
-}
+var homeTemplate *template.Template
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := homeView.Template.ExecuteTemplate(w,
-		homeView.Layout, nil)
-	if err != nil {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
 		panic(err)
 	}
-
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := contactView.Template.ExecuteTemplate(w,
-		contactView.Layout, nil)
+	fmt.Fprint(w, "To get in touch please contact site admin")
+}
+
+func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
 	if err != nil {
 		panic(err)
 	}
-
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Here are some Frequently Asked Questions:</h1>")
-}
-
-func hiross(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hi Ross!</h1>")
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
