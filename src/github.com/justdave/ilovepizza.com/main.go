cc1421780dc9bbeb5ea9dfb363dd6a30aa9b6480
Rohan <rohan@Rohans-MacBook-Air.local>
2018-01-26 12:27:27 -0600

    working csv dump with dummy info

--- src/github.com/justdave/ilovepizza.com/main.go
+++ src/github.com/justdave/ilovepizza.com/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -66,7 +68,19 @@ func delete(w http.ResponseWriter, req *http.Request) {
 }
 
 func dumpcsv(w http.ResponseWriter, req *http.Request) {
+	file, err := os.OpenFile("test.csv", os.O_CREATE|os.O_WRONLY, 0777)
+	defer file.Close()
 
+	if err != nil {
+		os.Exit(1)
+	}
+	x := []string{"Country", "City", "Population"}
+	y := []string{"Japan", "Tokyo", "923456"}
+	z := []string{"Australia", "Sydney", "789650"}
+	csvWriter := csv.NewWriter(file)
+	strWrite := [][]string{x, y, z}
+	csvWriter.WriteAll(strWrite)
+	csvWriter.Flush()
 }
 
 func grabcsv(w http.ResponseWriter, req *http.Request) {
