45f4899a00d5860aa94ca934c68fb3d18d2867b2
Rohan <rohan@Rohans-MacBook-Air.local>
2018-02-05 03:07:57 -0600

    Refactored redis app

--- src/github.com/justdave/redis go practice/main.go
+++ src/github.com/justdave/redis go practice/main.go
@@ -2,67 +2,64 @@ package main
 
 import (
 	"fmt"
-	// Import the Radix.v2 redis package.
 	"log"
+	"strconv"
 
 	"github.com/mediocregopher/radix.v2/redis"
 )
 
+// Define a custom struct to hold Album data.
+type Album struct {
+	Title  string
+	Artist string
+	Price  float64
+	Likes  int
+}
+
 func main() {
-	// Establish a connection to the Redis server listening on port 6379 of the
-	// local machine. 6379 is the default port, so unless you've already
-	// changed the Redis configuration file this should work.
 	conn, err := redis.Dial("tcp", "localhost:6379")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Importantly, use defer to ensure the connection is always properly
-	// closed before exiting the main() function.
 	defer conn.Close()
 
-	// Send our command across the connection. The first parameter to Cmd()
-	// is always the name of the Redis command (in this example HMSET),
-	// optionally followed by any necessary arguments (in this example the
-	// key, followed by the various hash fields and values).
-	resp := conn.Cmd("HMSET", "album:1", "title", "Electric Ladyland", "artist", "Jimi Hendrix", "price", 4.95, "likes", 8)
-	// Check the Err field of the *Resp object for any errors.
-
-	if resp.Err != nil {
-		log.Fatal(resp.Err)
-	}
-
-	err = conn.Cmd("HMSET", "album:1", "title", "Electric Ladyland", "artist", "Jimi Hendrix", "price", 4.95, "likes", 8).Err
-
+	// Fetch all album fields with the HGETALL command. Because HGETALL
+	// returns an array reply, and because the underlying data structure in
+	// Redis is a hash, it makes sense to use the Map() helper function to
+	// convert the reply to a map[string]string.
+	reply, err := conn.Cmd("HGETALL", "album:1").Map()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Electric Ladyland added!")
-
-	// Issue a HGET command to retrieve the title for a specific album, and use
-	// the Str() helper method to convert the reply to a string.
-	title, err := conn.Cmd("HGET", "album:1", "title").Str()
+	// Use the populateAlbum helper function to create a new Album object from
+	// the map[string]string.
+	ab, err := populateAlbum(reply)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Similarly, get the artist and convert it to a string.
-	artist, err := conn.Cmd("HGET", "album:1", "artist").Str()
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(ab)
+}
 
-	// And the price as a float64...
-	price, err := conn.Cmd("HGET", "album:1", "price").Float64()
+// Create, populate and return a pointer to a new Album struct, based on data
+// from a map[string]string.
+func populateAlbum(reply map[string]string) (*Album, error) {
+	var err error
+	ab := new(Album)
+	ab.Title = reply["title"]
+	ab.Artist = reply["artist"]
+	// We need to use the strconv package to convert the 'price' value from a
+	// string to a float64 before assigning it.
+	ab.Price, err = strconv.ParseFloat(reply["price"], 64)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-
-	// And the number of likes as an integer.
-	likes, err := conn.Cmd("HGET", "album:1", "likes").Int()
+	// Similarly, we need to convert the 'likes' value from a string to an
+	// integer.
+	ab.Likes, err = strconv.Atoi(reply["likes"])
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-
-	fmt.Printf("%s by %s: £%.2f [%d likes]\n", title, artist, price, likes)
+	return ab, nil
 }
