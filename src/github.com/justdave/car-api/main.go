abca535d6d89e5435266b4c5e855ba77c04b57ab
Dave <justdave@Daves-MacBook-Air.local>
2018-04-06 15:19:54 -0400

    updated car-api

--- src/github.com/justdave/car-api/main.go
+++ src/github.com/justdave/car-api/main.go
@@ -2,63 +2,110 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-type engine struct {
+//Garage is the Scuderia for the Cars
+type Garage struct {
+	Capacity int
+	Cars     []Car
+}
+
+// Engine is the basic part of the Car
+type Engine struct {
 	Configuration string
 	EPARating     string
+	Mileage       int
 }
 
+//Car is the abstract part of the Car
+type Car interface {
+	getMileage() int
+	getEngine() Engine
+	Start()
+	Drive()
+	Stop()
+	Maintain()
+}
+
+// car is the abstract part of the Car
 type car struct {
-	engine
-	Miles int
+	Engine Engine
 }
 
-type lamborghini struct {
-	car
-	Owner string
+func (c car) getMileage() int {
+	return c.Engine.Mileage
 }
 
-type porsche struct {
+func (c car) getEngine() Engine {
+	return c.Engine
+}
+
+func (c car) Start() {
+	panic("implement me")
+}
+
+func (c car) Drive() {
+	panic("implement me")
+}
+
+func (c car) Stop() {
+	panic("implement me")
+}
+
+func (c car) Maintain() {
+	panic("implement me")
+}
+
+//Ferrari is the more complete version
+type Ferrari struct {
 	car
 	Owner string
+	Model string
+}
+
+//Lamborghini is another version of Car
+type Lamborghini struct {
+	car
+	Owner string
+	Model string
 }
 
 func fly(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "I am but a leaf in the wind watch me Soar!")
 }
 
-func print(w http.ResponseWriter, r *http.Request) {
+func printCar(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	gallardo := lamborghini{
-		car{
-			engine{
-				"V10",
-				"5 MPG",
+	jaylenogarage := Garage{
+		Capacity: 100,
+		Cars: []Car{
+			&Lamborghini{
+				car: car{
+					Engine: Engine{
+						Mileage: 1200,
+					},
+				},
+				Owner: "ME",
 			},
-			1200,
+			&Ferrari{
+				car: car{
+					Engine: Engine{
+						Mileage: 1200,
+					},
+				},
+				Owner: "DAVE",
+			},
 		},
-		"Kanye West",
 	}
 
-	carrera := porsche{
-		car{
-			engine{
-				"Twin Turbo V6",
-				"22 MPG",
-			},
-			14386,
-		},
-		"Some Doctor Guy",
+	for _, c := range jaylenogarage.Cars {
+		fmt.Printf("%#v\n", c)
 	}
 
-	r := mux.NewRouter()
-	r.HandleFunc("/fly", fly).Methods("GET")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	//r := mux.NewRouter()
+	//r.HandleFunc("/fly", fly).Methods("GET")
+	//log.Fatal(http.ListenAndServe(":3000", r))
 }
