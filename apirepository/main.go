3c560b5a481b903c20698f057ab8a162fe8bd2fd
Dave Banerjee <justdave@Daves-MacBook-Pro.local>
2019-03-06 09:43:56 -0600

    added in apirepository exercise

---
+++ apirepository/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type repositories struct {
+	Name  string `json:"full_name"`
+	Owner owner  `json:"owner"`
+}
+
+type owner struct {
+	URL string `json:"url"`
+}
+
+func main() {
+	fmt.Println("Hello")
+
+	name, err := http.Get("https://api.github.com/repositories")
+
+	if err != nil {
+		log.Printf("name url error: %v", err)
+	}
+
+	body, err := ioutil.ReadAll(name.Body)
+
+	n, err := getInfo(body)
+
+	if err != nil {
+		log.Printf("name body read error: %v", err)
+	}
+
+	fmt.Println(n)
+}
+
+var repo []repositories
+
+func getInfo(body []byte) ([]repositories, error) {
+	n := make([]repositories, 0)
+	err := json.Unmarshal(body, &n)
+	if err != nil {
+		log.Printf("name unmarshal error: %v", err)
+	}
+	return n, nil
+}
